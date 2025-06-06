ec00922551c5235b6827607d99b1d3d3782a61d7
Michael Vitali <m.vitali24@imperial.ac.uk>
2025-06-06 16:16:59 +0200

    update pov

--- payoff/proportion_variance.jl
+++ payoff/proportion_variance.jl
@@ -6,7 +6,7 @@ export proportion_variance_payoff
 
     function proportion_variance_payoff(weights)
 
-        phis = weights ./ sum(weights)
+        phis = weights.^2 ./ sum(weights.^2)
         return phis
     end
 
