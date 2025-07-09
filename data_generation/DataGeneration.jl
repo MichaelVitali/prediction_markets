54a6324e7b5479618c4e4e08cce754c0b8baddd6
Michael Vitali <m.vitali24@imperial.ac.uk>
2025-07-09 14:57:48 +0100

    new dynamic environment

--- data_generation/DataGeneration.jl
+++ data_generation/DataGeneration.jl
@@ -3,7 +3,7 @@ using Statistics
 using Distributions
 using LinearAlgebra
 
-export data_generation_case_study_abrupt, data_generation_case_study_invariant, generate_time_invariant_data, data_generation_case_study_dynamic, generate_abrupt_data, generate_dynamic_data
+export data_generation_case_study_abrupt, data_generation_case_study_invariant, generate_time_invariant_data, data_generation_case_study_dynamic, generate_abrupt_data, generate_dynamic_data, generate_dynamic_data_sin
 
     function data_generation_case_study_invariant(case_study, T, q)
 
@@ -257,6 +257,55 @@ export data_generation_case_study_abrupt, data_generation_case_study_invariant, 
             else
                 w  = lambda .* w + (1-lambda) .* w2
             end
+            
+            mu_y = mu1[t] .* w[1] .+ mu2[t] .* w[2] .+ mu3[t] .* w[3]
+            sig_y = w[1] * sig1 + w[2] * sig2 + w[3] * sig3
+            Y = Normal(mu_y, sig_y)
+            true_values[t] = rand(Y)
+
+            true_weights[t, :] = w
+        end
+
+        f1 = quantile.(F1, q)
+        f2 = quantile.(F2, q)
+        f3 = quantile.(F3, q)
+
+        forecasters_dict = Dict("f1" => f1, "f2" => f2, "f3" => f3)
+            
+        return true_values, forecasters_dict, true_weights
+    end
+
+    function generate_dynamic_data_sin(T, q, n=4)
+
+        mu1 = zeros(T) .+ randn(T)*0.5
+        mu2 = fill(1, T) .+ randn(T)*0.5
+        mu3 = fill(2, T) .+ randn(T)*0.5
+        sig1 = 1
+        sig2 = 1
+        sig3 = 1
+
+        F1 = Normal.(mu1, sig1)
+        F2 = Normal.(mu2, sig2)
+        F3 = Normal.(mu3, sig3)
+
+        true_values = zeros(T)
+        true_weights = Matrix{Float16}(undef, T, 3)
+        w1 = [0.1, 0.6, 0.3]
+        w2 = [0.4, 0.2, 0.4]
+        lambda = 0.999
+        w = [1/3, 1/3, 1/3]
+
+        for t in 1:T
+
+            alpha = 0.5 * (1 .+ sin(2 * pi * n * t / T))  # n full cycles over time T
+
+            # Interpolate between w1 and w2 using alpha
+            w_target = (1 .- alpha) .* w1 .+ alpha .* w2
+
+            # Apply exponential smoothing to approach the target smoothly
+            w = lambda .* w .+ (1 - lambda) .* w_target
+
+            true_weights[t, :] = w
             
             mu_y = mu1[t] .* w[1] .+ mu2[t] .* w[2] .+ mu3[t] .* w[3]
             sig_y = w[1] * sig1 + w[2] * sig2 + w[3] * sig3
