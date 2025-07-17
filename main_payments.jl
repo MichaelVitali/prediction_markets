15f4e989fb104a710df7b8d9840c199577996144
Michael Vitali <m.vitali24@imperial.ac.uk>
2025-07-17 10:42:12 +0100

    in-out sample rewards implementation

--- main_payments.jl
+++ main_payments.jl
@@ -3,6 +3,7 @@ using Plots
 using Statistics
 using DataStructures
 using Base.Threads
+using LossFunctions
 
 include("functions/functions.jl")
 include("functions/functions_payoff.jl")
@@ -20,18 +21,20 @@ using .Shapley
 using .AdaptiveRobustRegression
 
 # Environment Settings
-q = 0.9
+q = 0.5
 n_forecasters = 3
 algorithms = ["QR", "RQR"]
 payoff_functions = "Shapley"
 daily_reward = 100
 T = 10000
-n_experiments = 500
+n_experiments = 100
+delta = 0.7
 
 # Environment Variables
 payoffs = Dict([algo => zeros(n_forecasters, T) for algo in algorithms])
 rewards = Dict([algo => zeros(n_forecasters, T) for algo in algorithms])
 weights = Dict([algo => zeros(n_forecasters, T) for algo in algorithms])
+scores = Dict([algo => zeros(n_forecasters, T) for algo in algorithms])
 
 # Environment generation
 ## Weights initialization
@@ -47,6 +50,7 @@ Threads.@threads for exp in 1:n_experiments
     weights_exp = zeros(n_forecasters, T)
     payoffs_exp = zeros(n_forecasters, T)
     rewards_exp = zeros(n_forecasters, T)
+    scores_exp = zeros(n_forecasters, T)
 
     weights_exp[:, 1] = initialize_weights(n_forecasters)
 
@@ -61,19 +65,36 @@ Threads.@threads for exp in 1:n_experiments
 
         weights_exp[:, t], _ = online_quantile_regression_update(forecasters_preds_t, weights_exp[:, t-1], y_true, q, 0.01)
         temp_payoffs = shapley_payoff(forecasters_preds_t, weights_exp[:, t-1], y_true, q)
+        temp_scores = 1 .- (QuantileLoss(q).(forecasters_preds_t, y_true) ./ sum(QuantileLoss(q).(forecasters_preds_t, y_true)))
         payoffs_exp[:, t] = payoff_update(payoffs_exp[:, t-1], temp_payoffs, 0.999)
+        scores_exp[:, t] = payoff_update(scores_exp[:, t-1], temp_scores, 0.999)
 
-        rewards_exp[:, t] = daily_reward .* (max.(0, payoffs_exp[:, t]) ./ sum(max.(0, payoffs_exp[:, t])))
+        rewards_in = delta .* daily_reward .* (max.(0, payoffs_exp[:, t]) ./ sum(max.(0, payoffs_exp[:, t])))
+        rewards_out = (1-delta) .* daily_reward .* (temp_scores ./ sum(temp_scores))
+        rewards_exp[:, t] = rewards_in .+ rewards_out
+
     end
 
     payoffs[algo] += payoffs_exp
     weights[algo] += weights_exp
     rewards[algo] += rewards_exp
+    scores[algo] += scores_exp
 end
 
 payoffs[algo] = payoffs[algo] ./ n_experiments
 weights[algo] = weights[algo] ./ n_experiments
 rewards[algo] = rewards[algo] ./ n_experiments
+scores[algo] = scores[algo] ./ n_experiments
+
+scores[algo] = scores[algo] ./ sum(scores[algo], dims=1)
+
+plot_scores = plot(size=(1000, 400))
+plot!(plot_scores, 1:T, rewards["QR"]', label=["Forecaster 1" "Forecaster 2" "Forecaster 3"], lw=2)
+xlabel!(plot_scores, "Time")
+ylabel!(plot_scores, "Reward")
+title!(plot_scores, "Rewards for All Forecasters (QR)")
+display(plot_scores)
+
 
 #################### Robust Quantile Regression ####################
 algo = "RQR"
@@ -94,7 +115,7 @@ Threads.@threads for exp in 1:n_experiments
 
     D_exp = zeros(n_forecasters, n_forecasters)
     alpha = zeros(n_forecasters, T)
-    alpha[missing_forecast, :] = Int.(rand(T) .< 0.9)
+    alpha[missing_forecast, :] = Int.(rand(T) .< 0.05)
 
     for t in 2:T
         forecasters_preds_t = [forecasters_preds[f][t] for f in keys(sorted_forecasters)]
@@ -111,15 +132,26 @@ Threads.@threads for exp in 1:n_experiments
         temp_weights_t = [temp_weights_t[j] for j in 1:length(forecasters_preds_t) if alpha[j, t] == 0]
 
         temp_payoffs = shapley_payoff(temp_forecasts_t, temp_weights_t, y_true, q)
+        temp_scores = 1 .- (QuantileLoss(q).(temp_forecasts_t, y_true) ./ sum(QuantileLoss(q).(temp_forecasts_t, y_true)))
         if length(temp_payoffs) < n_forecasters
             insert!(temp_payoffs, missing_forecast, 0.0)
+            insert!(temp_scores, missing_forecast, 0.0)
         end
         payoffs_exp[:, t] = payoff_update(payoffs_exp[:, t-1], temp_payoffs, 0.999)
 
         # Reward calculation
-        payoffs_for_rewards = [max(0, payoffs_exp[j, t]) for j in 1:length(forecasters_preds_t) if alpha[j, t] == 0]
-        rewards_exp[missing_forecast, t] = 0.0
-        rewards_exp[[j for j in 1:n_forecasters if alpha[j, t] == 0], t] = daily_reward .* (payoffs_for_rewards ./ sum(payoffs_for_rewards))
+        rewards_in = zeros(n_forecasters)
+        rewards_out = zeros(n_forecasters)
+
+        payoffs_for_rewards_in = [max(0, payoffs_exp[j, t]) for j in 1:length(forecasters_preds_t) if alpha[j, t] == 0]
+        rewards_in[missing_forecast] = 0.0
+        rewards_in[[j for j in 1:n_forecasters if alpha[j, t] == 0]] = delta .* daily_reward .* (payoffs_for_rewards_in ./ sum(payoffs_for_rewards_in))
+
+        scores_for_rewards_out = [temp_scores[j] for j in 1:length(forecasters_preds_t) if alpha[j, t] == 0]
+        rewards_out[missing_forecast] = 0.0
+        rewards_out[[j for j in 1:n_forecasters if alpha[j, t] == 0]] = (1-delta) .* daily_reward .* (scores_for_rewards_out / sum(scores_for_rewards_out))
+
+        rewards_exp[:, t] = rewards_in .+ rewards_out
     end
 
     payoffs[algo] += payoffs_exp
@@ -130,25 +162,43 @@ end
 payoffs[algo] = payoffs[algo] ./ n_experiments
 weights[algo] = weights[algo] ./ n_experiments
 rewards[algo] = rewards[algo] ./ n_experiments
+display(rewards[algo])
 
-
-#window = 9999
-window = 1000
-plot_rewards = plot(layout=(n_forecasters-1, 1), size=(1000, 500))
-
-for forecaster_idx in 1:n_forecasters
-    if forecaster_idx == missing_forecast
-        continue
+for window in [1000, 9999]
+    #plot_rewards = plot(layout=(n_forecasters-1, 1), size=(1000, 500))
+    plot_rewards = plot(layout=(n_forecasters, 1), size=(1000, 500))
+    for forecaster_idx in 1:n_forecasters
+        #if forecaster_idx == missing_forecast
+        #    continue
+        #end
+        #i = forecaster_idx - (forecaster_idx > missing_forecast ? 1 : 0)
+        i = forecaster_idx
+        plot!(plot_rewards[i], T-window:T, payoffs["QR"][forecaster_idx, end-window:end], label="QR - Forecaster $forecaster_idx", lw=2)
+        plot!(plot_rewards[i], T-window:T, payoffs["RQR"][forecaster_idx, end-window:end], label="RQR - Forecaster $forecaster_idx", lw=2)
+        xlabel!(plot_rewards[i], "Time")
+        ylabel!(plot_rewards[i], "Value")
+        title!(plot_rewards[i], "Shapley Forecaster $forecaster_idx (window=$window)")
     end
-    i = forecaster_idx - (forecaster_idx > missing_forecast ? 1 : 0)
-    plot!(plot_rewards[i], T-window:T, rewards["QR"][forecaster_idx, end-window:end], label="QR - Forecaster $forecaster_idx", lw=2)
-    plot!(plot_rewards[i], T-window:T, rewards["RQR"][forecaster_idx, end-window:end], label="RQR - Forecaster $forecaster_idx", lw=2)
-    xlabel!(plot_rewards[i], "Time")
-    ylabel!(plot_rewards[i], "£")
-    title!(plot_rewards[i], "Reward Forecaster $forecaster_idx")
+    display(plot_rewards)
 end
 
-display(plot_rewards)
+for window in [9999]
+    #plot_rewards = plot(layout=(n_forecasters-1, 1), size=(1000, 500))
+    plot_rewards = plot(layout=(n_forecasters, 1), size=(1000, 500))
+    for forecaster_idx in 1:n_forecasters
+        #if forecaster_idx == missing_forecast
+        #    continue
+        #end
+        #i = forecaster_idx - (forecaster_idx > missing_forecast ? 1 : 0)
+        i = forecaster_idx
+        plot!(plot_rewards[i], T-window:T, rewards["QR"][forecaster_idx, end-window:end], label="QR - Forecaster $forecaster_idx", lw=2)
+        plot!(plot_rewards[i], T-window:T, rewards["RQR"][forecaster_idx, end-window:end], label="RQR - Forecaster $forecaster_idx", lw=2)
+        xlabel!(plot_rewards[i], "Time")
+        ylabel!(plot_rewards[i], "£")
+        title!(plot_rewards[i], "Rewards Forecaster $forecaster_idx (window=$window)")
+    end
+    display(plot_rewards)
+end
 
 plot_weigths = plot(layout=(length(algorithms), 1), size=(1000, 500))
 for (i, algo) in enumerate(algorithms)
